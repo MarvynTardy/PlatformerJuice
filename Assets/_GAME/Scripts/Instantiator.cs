fd62b78039add00061e2e035a5a3d0d15b97a193
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2020-12-11 13:49:11 +0100

    instantiator

---
+++ Assets/_GAME/Scripts/Instantiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Prefab = null;
+    public void InstantiatePrefab(GameObject p_Prefab)
+    {
+        Instantiate(p_Prefab);
+    }
+
+    public void InstantiatePrefab(CollectInfos p_Infos)
+    {
+        Instantiate(m_Prefab, p_Infos.position, Quaternion.identity);
+    }
+}
